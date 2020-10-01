1c2a3104c5e4a79ccee742fd5e7f3565b19ea9e9
Deviprasad Chand <DeviprasadC1@verifone.com>
2020-10-01 14:32:46 +0530

    initial commit for original class with 2 other classes mentioned in the same java file

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Original Main Class");
+	}
+}
+
+class TestClass1 {
+	public void print() {
+		System.out.println("TestClass1_original");
+	}
+}
+
+class TestClass2 {
+	public void print() {
+		System.out.println("TestClass2_original");
+	}
+}
